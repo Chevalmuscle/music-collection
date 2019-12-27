0f4fe608b8d8c2f1f3e7d88ca5e093291fbd8343
chevalmuscle <marcantoinejean1997@gmail.com>
2019-12-27 15:10:25 -0500

    Added routing

--- src/index.js
+++ src/index.js
@@ -1,7 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Route, BrowserRouter as Router } from "react-router-dom";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import * as serviceWorker from "./serviceWorker";
+
+import App from "./App";
+import Albums from "./pages/Albums";
+import Album from "./pages/Album";
+
+const routing = (
+  <Router>
+    <div>
+      <Route exact path="/" component={App} />
+      <Route exact path="/albums" component={Albums} />
+      <Route exact path="/albums/:id" component={Album} />
+    </div>
+  </Router>
+);
+
+ReactDOM.render(routing, document.getElementById("root"));
 serviceWorker.unregister();
